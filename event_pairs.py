608efd643fcd415558544f027e191401d8fc82aa
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-24 14:05:23 +0200

    update

--- event_pairs.py
+++ event_pairs.py
@@ -11,48 +11,42 @@ class Event_pairs:
                 self.tweets.append(tweet)
 
     def extract_date(self,new_tweets):
-        convert_nums = {"een":1, "twee":2, "drie":3, "vier":4, "vijf":5, "zes":6, "zeven":7, 
-            "acht":8, "negen":9, "tien":10, "elf":11, "twaalf":12, "dertien":13, "veertien":14, 
-            "vijftien":15, "zestien":16, "zeventien":17, "achtien":18, "negentien":19, 
-            "twintig":20,"eenentwintig":21,"tweeentwintig":22,"drieentwintig":23,"vierentwintig":24,
-            "vijfentwintig":25,"zesentwintig":26,"zevenentwintig":27,"achtentwintig":28,
-            "negenentwintig":29,"dertig":30}
-        convert_timeunit = {"dagen":1, "daagjes":1, "nachten":1, "nachtjes":1, "week": 7, "weken":7, 
-            "weekjes":7, "maand": 30, "maanden":30, "maandjes":30}
-        nums = (r"(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|tien|elf|twaalf|dertien|" 
-            "veertien|vijftien|zestien|zeventien|achtien|negentien|twintig|eenentwintig|"
-            "tweeentwintig|drieentwintig|vierentwintig|vijfentwintig|zesentwintig|"
-            "zevenentwintig|achtentwintig|negenentwintig|dertig)")
-        timeunits = (r"(dag|dagje|dagen|daagjes|nacht|nachtje|nachten|nachtjes|week|weekje|weken|"
-            "weekjes|maand|maandje|maanden|maandjes)")
+        convert_nums = {"een":1, "twee":2, "drie":3, "vier":4,
+            "vijf":5, "zes":6, "zeven":7, "acht":8, "negen":9, 
+            "tien":10, "elf":11, "twaalf":12, "dertien":13,
+            "veertien":14, "vijftien":15, "zestien":16, "zeventien":17,
+            "achtien":18, "negentien":19, "twintig":20,
+            "eenentwintig":21,"tweeentwintig":22,"drieentwintig":23,
+            "vierentwintig":24,"vijfentwintig":25,"zesentwintig":26,
+            "zevenentwintig":27,"achtentwintig":28,"negenentwintig":29,
+            "dertig":30}
+        convert_timeunit = {"dagen":1, "daagjes":1, "nachten":1, 
+            "nachtjes":1, "week": 7, "weken":7, "weekjes":7,
+            "maand": 30, "maanden":30, "maandjes":30}
+        nums = (r"(\d+|een|twee|drie|vier|vijf|zes|zeven|acht|negen|"
+            "tien|elf|twaalf|dertien|veertien|vijftien|zestien|"
+            "zeventien|achtien|negentien|twintig|eenentwintig|"
+            "tweeentwintig|drieentwintig|vierentwintig|vijfentwintig|"
+            "zesentwintig|zevenentwintig|achtentwintig|negenentwintig|"
+            "dertig)")
+        timeunits = (r"(dag|dagje|dagen|daagjes|nacht|nachtje|nachten|"
+            "nachtjes|week|weekje|weken|weekjes|maand|maandje|maanden|"
+            "maandjes)")
         # check = re.compile(r"\b(dagen|daagjes|nachten|nachtjes|weken|weekjes|maanden|maandjes)\b",re.IGNORECASE)
-        days = re.compile(r"(over|nog) (minimaal |maximaal |tenminste |"
+        m1 = re.compile(r"(over|nog) (minimaal |maximaal |tenminste |"
             "bijna |ongeveer |maar |slechts |pakweg |ruim |krap |"
             "(maar )?een kleine |(maar )?iets (meer|minder) dan )?" + 
             (nums) + " " + (timeunits))
-        # days1 = re.compile(r"(over|nog) pakweg " + (nums) + " " + (timeunits),re.IGNORECASE)
-        # days2 = re.compile(r"nog slechts (een kleine )?" + (nums) + " " + (timeunits),re.IGNORECASE)
-        days3 = re.compile((nums) + " " + (timeunits) + r"( slapen)? tot")
-        # days4 = re.compile(r"(met )?nog (een kleine |maar |slechts )?" + (nums) + " " + (timeunits),re.IGNORECASE)
-        # days5 = re.compile(r"nog (maar )?" + (nums) + " " + (timeunits),re.IGNORECASE)
-        # days6 = re.compile((nums) + " " + (timeunits) + r"( slapen)? tot",re.IGNORECASE)
-        # days7 = re.compile(r"(over|nog) " + (nums) + " " + (timeunits),re.IGNORECASE)
-        # days8 = re.compile(r"(over|nog) (ruim|krap|een kleine|ongeveer|bijna) " + (nums) + " " + (timeunits),re.IGNORECASE)
-        # days9 = re.compile(r"(over|nog) (maar |slechts |minimaal |maximaal |tenminste )?" + (nums) + " " + (timeunits),re.IGNORECASE)
-        days10 = re.compile(r"met( nog)? (minimaal |maximaal |tenminste |"
+        m2 = re.compile((nums) + " " + (timeunits) + r"( slapen)? tot")
+        m3 = re.compile(r"met( nog)? (minimaal |maximaal |tenminste |"
             "bijna |ongeveer |maar |slechts |pakweg |ruim |krap |"
             "(maar )?een kleine |(maar )?iets (meer|minder) dan )?" + 
             (nums) + " " + (timeunits) + r"( nog)? te gaan")
-        # days11 = re.compile(r"met (nog)? (maar|slechts)?( een| 1) (dag|week|maand) te gaan",re.IGNORECASE)
 
         lines = []
         for tweet in new_tweets:
             text = tweet.strip().split("\t")[-1].lower()
-            if days.search(text):
-                lines.append(text)
-            elif days3.search(text):
-                lines.append(text)
-            elif days10.search(text):
+            if m1.search(text) or m2.search(text) or m3.search(text):
                 lines.append(text)
         print lines,len(lines)
 
