a681646ed8773677c52b78645b659c4839e1832f
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-25 13:58:55 +0200

    update

--- score_commonness_wikidump.py
+++ score_commonness_wikidump.py
@@ -7,6 +7,10 @@ import xml.etree.ElementTree as etree
 tmp = sys.argv[1]
 wiki = sys.argv[2]
 
+list_patterns = (["{{","}}","^:","^;","^\#","^:","^<","^afbeelding:",
+    'bestand:','==','\*',"\|{2}","\|-","class=","categorie:",
+    '^[\!\|]'])
+
 #for each page
 for event, elem in etree.iterparse(wiki, events=('start', 'end', 'start-ns', 'end-ns')):
     if event == 'end':
@@ -15,7 +19,7 @@ for event, elem in etree.iterparse(wiki, events=('start', 'end', 'start-ns', 'en
             #print elem.text
             b = elem.text.lower().split("\n")
             for u in b:
-                if re.match(r'^\s*$',u) or re.search("}}",u) or re.search("^:",u) or re.search("^;",u) or re.search("^\#",u) or re.search("math>",u) or re.search("{{",u) or re.search('bestand:',u) or re.search('==',u) or re.search('\*',u) or re.search(r"\|{2}",u) or re.search("\|-",u) or re.search("class=",u) or re.search("categorie:",u) or re.search('^[\!\|]',u):
+                if re.match(r'^\s*$',u) or re.findall('|'.join(list_patterns),u):
                     continue
                 else:
                     print u
