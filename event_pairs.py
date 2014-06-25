8b4f859afe231b737437b5f491f2aebc57007196
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-25 10:28:09 +0200

    fix

--- event_pairs.py
+++ event_pairs.py
@@ -16,15 +16,16 @@ class Event_pairs:
     def select_date_tweets(self,new_tweets):
         for tweet in new_tweets:
             tokens = tweet.strip().split("\t")
-            text = tokens[-1].lower()
-            date = time_functions.return_datetime(tokens[3],
-                setting="vs")
-            dateref = self.extract_date(text,date)
-            if dateref:
-                dtweet = self.Tweet()
-                dtweet.set_date(dateref)
-                self.tweets.append(dtweet)
-        print self.tweets
+            if tokens[0] == "dutch":
+                text = tokens[-1].lower()
+                date = time_functions.return_datetime(tokens[3],
+                    setting="vs")
+                dateref = self.extract_date(text,date)
+                if dateref:
+                    dtweet = self.Tweet()
+                    dtweet.set_rdate(dateref)
+                    self.tweets.append(dtweet)
+        print [x.rdate for x in self.tweets]
 
     def extract_date(self,tweet,date):
 
@@ -135,15 +136,22 @@ class Event_pairs:
                         ds = date_eu.search(da + nud["year"]).groups()
                     else:
                         ds = date_eu.search(da).groups()
-                    print ds,tweet
-                    return datetime.date(int(ds[2]),int(ds[1]),int(ds[0]))
+                    dsi = [int(x) for x in ds]
+                    if dsi[2] in range(2010,2020) and 
+                        dsi[1] in range(1,13) and 
+                        dsi[0] in range(1,32):
+                        return datetime.date(ds[2],ds[1],
+                            dsi[0])
                 elif re.search("/",da):
                     if "year" in nud:
                         ds = date_vs.search(nud["year"] + da).groups()
                     else:
                         ds = date_vs.search(da).groups()
-                    print ds
-                    return datetime.date(int(ds[0]),int(ds[1]),int(ds[2]))
+                    dsi = [int(x) for x in ds]
+                    if dsi[0] in range(2010,2020) and 
+                        dsi[1] in range(1,13) and 
+                        dsi[2] in range(1,32):
+                        return datetime.date(dsi[0],dsi[1],dsi[2])
             else:
                 return False
             #print re.findall('|'.join(list_patterns), text),text
@@ -244,8 +252,8 @@ class Event_pairs:
             self.text = units[3]
             self.entities = units[4:]
 
-        def set_date(self,date):
-            self.date = date
+        def set_rdate(self,date):
+            self.rdate = date
 
         def set_entities(self,entities):
             self.entities = entities
