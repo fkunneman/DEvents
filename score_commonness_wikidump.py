e290e478d7b758afe4ee2c50744d2e4b03ef4c0f
Florian Kunneman <f.kunneman@gmail.com>
2014-06-25 13:48:08 +0200

    update

--- score_commonness_wikidump.py
+++ score_commonness_wikidump.py
@@ -1,5 +1,6 @@
 
 import sys
+import re
 import colibricore
 import xml.etree.ElementTree as etree
 
@@ -10,5 +11,14 @@ wiki = sys.argv[2]
 for event, elem in etree.iterparse(wiki, events=('start', 'end', 'start-ns', 'end-ns')):
     if event == 'end':
         if elem.tag == '{http://www.mediawiki.org/xml/export-0.8/}text':
-            print elem.text
+            print '****************************************************'
+            #print elem.text
+            b = elem.text.lower().split("\n")
+            for u in b:
+                if re.match(r'^\s*$',u) or re.search("}}",u) or re.search("^:",u) or re.search("^;",u) or re.search("^\#",u) or re.search("math>",u) or re.search("{{",u) or re.search('bestand:',u) or re.search('==',u) or re.search('\*',u) or re.search(r"\|{2}",u) or re.search("\|-",u) or re.search("class=",u) or re.search("categorie:",u) or re.search('^[\!\|]',u):
+                    continue
+                else:
+                    print u
+            print '****************************************************'
+            
 
