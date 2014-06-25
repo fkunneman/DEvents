89432e65f92c7661c903b399d9027b96a740aaba
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-25 17:02:48 +0200

    update

--- score_commonness_wikidump.py
+++ score_commonness_wikidump.py
@@ -2,36 +2,24 @@
 import sys
 import re
 import codecs
+import json
 import colibricore
-import xml.etree.ElementTree as etree
 
 tmp = sys.argv[1]
-wiki = sys.argv[2]
+infiles = sys.argv[2:]
 
-list_patterns = (["{{","}}","^:","^;","^\#","^:","^<","^afbeelding:",
-    'bestand:','==','\*',"\|{2}","\|-","class=","categorie:",
-    '^[\!\|]'])
-
-#for each page
-for event, elem in etree.iterparse(wiki, events=('start', 'end', 
-    'start-ns', 'end-ns')):
-    if event == 'end':
-        if elem.tag == '{http://www.mediawiki.org/xml/export-0.8/}text':
-            all_text = ""
-            b = elem.text.lower().split("\n")
-            for u in b:
-                if re.match(r'^\s*$',u) or re.findall('|'.join(list_patterns),u):
-                    continue
-                else:
-                    u = u.replace(',',' ,')
-                    u = u.replace('.',' .')
-                    u = u.replace(':',' :') 
-                    all_text = all_text + u
-
-            atfile = tmp + "page.txt"
-            f = codecs.open(atfile,'w','utf-8')
-            f.write(all_text)
-            f.close()
+for infile in infiles:
+    with codecs.open(infile,"r","utf-8") as f:
+        for l in f:
+            js = json.loads(l)
+            text = js["text"]
+            text = text.replace(',',' ,')
+            text = text.replace('.',' .')
+            text = text.replace(':',' :') 
+            textfile = tmp + "_page.txt"
+            tf = codecs.open(textfile,'w','utf-8')
+            tf.write(text)
+            tf.close()
             classfile = tmp + "page.colibri.cls"
             classencoder = colibricore.ClassEncoder()
             classencoder.build(atfile)
@@ -42,6 +30,6 @@ for event, elem in etree.iterparse(wiki, events=('start', 'end',
             for sentence in corpusdata.sentences():
                 for fivegram in sentence.ngrams(5):
                     print(fivegram.tostring(classdecoder))
-            print "***********************************************"
+    quit()
 
 
