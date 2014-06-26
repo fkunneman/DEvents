820db19c699f96a46a74af95d2b1254acaaae276
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-26 10:14:17 +0200

    fix

--- score_commonness_wikidump.py
+++ score_commonness_wikidump.py
@@ -1,15 +1,17 @@
 
 import sys
 import re
-import codecs
+
 import json
 import colibricore
 
 tmp = sys.argv[1]
 infiles = sys.argv[2:]
 
+
+
 for infile in infiles:
-    with codecs.open(infile,"r","utf-8") as f:
+    with open(infile,"r","utf-8") as f:
         for l in f:
             js = json.loads(l)
             text = js["text"]
@@ -17,19 +19,17 @@ for infile in infiles:
             text = text.replace('.',' .')
             text = text.replace(':',' :') 
             textfile = tmp + "_page.txt"
-            tf = codecs.open(textfile,'w','utf-8')
-            tf.write(text)
-            tf.close()
+            with open(textfile,'w','utf-8') as g:
+                tf.write(text)
             classfile = tmp + "page.colibri.cls"
             classencoder = colibricore.ClassEncoder()
             classencoder.build(atfile)
             classencoder.save(classfile)
             corpusfile = tmp + "page.colibri.dat"
-            classencoder.encodefile(atfile, corpusfile)
+            classencoder.encodefile(textfile, corpusfile)
+            classdecoder = colibricore.ClassDecoder(classfile)
             corpusdata = colibricore.IndexedCorpus(corpusfile)
             for sentence in corpusdata.sentences():
                 for fivegram in sentence.ngrams(5):
                     print(fivegram.tostring(classdecoder))
     quit()
-
-
