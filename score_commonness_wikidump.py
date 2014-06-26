31a13575626a615950fb110a7520481783550fdc
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-26 10:17:42 +0200

    fix

--- score_commonness_wikidump.py
+++ score_commonness_wikidump.py
@@ -10,26 +10,26 @@ infiles = sys.argv[2:]
 
 for infile in infiles:
     f = open(infile,"r","utf-8"):
-        for l in f.readlines():
-            js = json.loads(l)
-            text = js["text"]
-            text = text.replace(',',' ,')
-            text = text.replace('.',' .')
-            text = text.replace(':',' :') 
-            textfile = tmp + "_page.txt"
-            options = colibricore.PatternModelOptions(maxlength=7)
-            with open(textfile,'w','utf-8') as g:
-                tf.write(text)
-            classfile = tmp + "page.colibri.cls"
-            classencoder = colibricore.ClassEncoder()
-            classencoder.build(textfile)
-            classencoder.save(classfile)
-            corpusfile = tmp + "page.colibri.dat"
-            classencoder.encodefile(textfile, corpusfile)
-            classdecoder = colibricore.ClassDecoder(classfile)
-            corpusdata = colibricore.IndexedCorpus(corpusfile)
-            for sentence in corpusdata.sentences():
-                for fivegram in sentence.ngrams(5):
-                    print(fivegram.tostring(classdecoder))
-        f.close()
+    for l in f.readlines():
+        js = json.loads(l)
+        text = js["text"]
+        text = text.replace(',',' ,')
+        text = text.replace('.',' .')
+        text = text.replace(':',' :') 
+        textfile = tmp + "_page.txt"
+        options = colibricore.PatternModelOptions(maxlength=7)
+        with open(textfile,'w','utf-8') as g:
+            tf.write(text)
+        classfile = tmp + "page.colibri.cls"
+        classencoder = colibricore.ClassEncoder()
+        classencoder.build(textfile)
+        classencoder.save(classfile)
+        corpusfile = tmp + "page.colibri.dat"
+        classencoder.encodefile(textfile, corpusfile)
+        classdecoder = colibricore.ClassDecoder(classfile)
+        corpusdata = colibricore.IndexedCorpus(corpusfile)
+        for sentence in corpusdata.sentences():
+            for fivegram in sentence.ngrams(5):
+                print(fivegram.tostring(classdecoder))
+    f.close()
     quit()
