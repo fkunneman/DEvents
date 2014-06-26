e8856fda73211a0962e81497e5a180b8c67cb7bb
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-26 11:29:51 +0200

    update

--- score_commonness_wikidump.py
+++ score_commonness_wikidump.py
@@ -2,6 +2,7 @@
 import sys
 import re
 import string
+from collections import defaultdict
 
 import json
 import colibricore
@@ -9,16 +10,17 @@ import colibricore
 tmp = sys.argv[1]
 infiles = sys.argv[2:]
 
-options = colibricore.PatternModelOptions(mintokens=1,maxlength=7)
+five = defaultdict(lambda : {})
+four = defaultdict(lambda : {})
+three = defaultdict(lambda : {})
+two = defaultdict(lambda : {})
+one = defaultdict(lambda : {})
 exclude = set(string.punctuation)
 for infile in infiles:
     f = open(infile,encoding = "utf-8")
     for l in f.readlines():
         js = json.loads(l)
         text = js["text"].lower()
-        #text = text.replace(',',' ,')
-        #text = text.replace('.',' .')
-        #text = text.replace(':',' :')
         text = ''.join(ch for ch in text if ch not in exclude)
         textfile = tmp + "_page.txt"
         with open(textfile,'w',encoding='utf-8') as g:      
@@ -30,14 +32,10 @@ for infile in infiles:
         corpusfile = tmp + "page.colibri.dat"
         classencoder.encodefile(textfile, corpusfile)
         classdecoder = colibricore.ClassDecoder(classfile)
-        model = colibricore.UnindexedPatternModel()
-        model.train(corpusfile, options)
-        for pattern in model:
-            print(pattern.tostring(classdecoder))
-        # corpusdata = colibricore.IndexedCorpus(corpusfile)
-        # for sentence in corpusdata.sentences():
-        #     for fivegram in sentence.ngrams(5):
-        #         print(fivegram.tostring(classdecoder))
+        corpusdata = colibricore.IndexedCorpus(corpusfile)
+        for sentence in corpusdata.sentences():
+            for fivegram in sentence.ngrams(5):
+                print(fivegram.tostring(classdecoder).split("\n"))                
         anchors = js["annotations"]
         print([x["surface_form"].lower() for x in anchors])
     f.close()
