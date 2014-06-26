b1f09d537fa7ccb62dffb1519f47a3f9a9f13127
Florian Kunneman <f.kunneman@gmail.com>
2014-06-26 10:14:28 +0200

    fdf

--- score_commonness_wikidump.py
+++ score_commonness_wikidump.py
@@ -20,16 +20,27 @@ for infile in infiles:
             tf = codecs.open(textfile,'w','utf-8')
             tf.write(text)
             tf.close()
+            options = colibricore.PatternModelOptions(maxlength=7)
             classfile = tmp + "page.colibri.cls"
             classencoder = colibricore.ClassEncoder()
-            classencoder.build(atfile)
+            classencoder.build(textfile)
             classencoder.save(classfile)
+            print len(classencoder)
             corpusfile = tmp + "page.colibri.dat"
-            classencoder.encodefile(atfile, corpusfile)
-            corpusdata = colibricore.IndexedCorpus(corpusfile)
-            for sentence in corpusdata.sentences():
-                for fivegram in sentence.ngrams(5):
-                    print(fivegram.tostring(classdecoder))
+            classencoder.encodefile(textfile, corpusfile)
+            classdecoder = colibricore.ClassDecoder(classfile)
+            decoded = classdecoder.decodefile(corpusfile)
+            print decoded
+           
+            #corpusdata = colibricore.IndexedCorpus(corpusfile)
+            #model = colibricore.UnindexedPatternModel()
+            #model.train(corpusfile, options)
+            #for pattern in model:
+            #    print pattern.tostring(classdecoder)
+            #for sentence in corpusdata.sentences():
+            #    for trigram in sentence.ngrams(3):
+            #        print trigram.tostring()
+            #        print trigram.tostring(classdecoder)
     quit()
 
 
