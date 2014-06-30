935516d24e1e39b155339d940b13ae33c4e19e2e
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-30 15:33:38 +0200

    update

--- extract_events.py
+++ extract_events.py
@@ -12,6 +12,8 @@ parser.add_argument('-m', action = 'store', required = False,
     help = "The file with information on existing pairs")
 parser.add_argument('-w', action = 'store', nargs='+', required = False, 
     help = "The files with wikicores per n-gram")
+parser.add_argument('-d', action = 'store', required = False, 
+    help = "The tmp dict for pattern indexing")
 parser.add_argument('-o', action = 'store', required = True, 
     help = "File to write event pairs to")
 args = parser.parse_args() 
@@ -22,5 +24,5 @@ tweetfile = codecs.open(args.i,"r","utf-8")
 ep.select_date_tweets(tweetfile.readlines())
 tweetfile.close()
 print "extracting entities"
-ep.select_entity_tweets(args.w)
+ep.select_entity_tweets(args.d,args.w)
 
