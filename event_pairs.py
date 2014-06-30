bc5b59a6f77b453dcd98edac203a672ecb619209
Florian Kunneman <f.kunneman@let.ru.nl>
2014-06-30 12:30:08 +0200

    fix

--- event_pairs.py
+++ event_pairs.py
@@ -140,10 +140,11 @@ class Event_pairs:
                     elif re.match(r"\d+",unit):
                         if int(unit) in range(2010,2020):
                             nud["year"].append((int(unit),i))
-                        elif "num" in nud and len(x for x in \
-                            nud["num"] if x[1] == i) > 0: 
-                            if int(unit) in range(1,13):
-                                nud["month"].append((int(unit),i))
+                        elif "num" in nud:
+                            if len(x for x in nud["num"] if x[1] \
+                                == i) > 0: 
+                                if int(unit) in range(1,13):
+                                    nud["month"].append((int(unit),i))
                         else:
                             nud["num"].append((int(unit),i))
                     elif unit in weekdays:
