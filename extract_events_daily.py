371bb7167a1fb6b7732d67bbe289746222a4016b
Florian Kunneman <f.kunneman@let.ru.nl>
2014-07-01 23:42:44 +0200

    fix

--- extract_events_daily.py
+++ extract_events_daily.py
@@ -40,34 +40,34 @@ quit()
 
 ep = Event_pairs()
 
-    print("extracting tweets with a time reference")
-    for infile in args.i:
-        tweetfile = open(infile,"r",encoding = "utf-8")
-        ep.select_date_tweets(tweetfile.readlines())
-        tweetfile.close()
-if args.w:
-    print("extracting entities")
-    ep.select_entity_tweets(args.d,args.w,args.a)
-if args.t:
-    ep.select_hashtags_tweets()
-print("ranking events")
-ranked_events = ep.rank_events()
-#print(ranked_events)
-print("writing output")
-tweetinfo = open(args.p,"w",encoding = "utf-8")
-for tweet in ep.tweets:
-    info = [tweet.id,tweet.user,str(tweet.date),tweet.text," ".join([str(x) for x in tweet.daterefs]),"|".join([x for x in tweet.chunks])]
-    try:
-        info.append(" ".join(tweet.entities))
-        tweetinfo.write("\t".join(info) + "\n")
-    except:
-        tweetinfo.write("\t".join(info) + "\n")
-tweetinfo.close()
-eventinfo = open(args.o,"w",encoding = "utf-8")
-for event in ranked_events:
-    outstr = "\n" + "\t".join([str(x) for x in event[:-1]]) + "\n" + "\n".join(event[-1]) + "\n"
-    eventinfo.write(outstr)
-eventinfo.close()
+#     print("extracting tweets with a time reference")
+#     for infile in args.i:
+#         tweetfile = open(infile,"r",encoding = "utf-8")
+#         ep.select_date_tweets(tweetfile.readlines())
+#         tweetfile.close()
+# if args.w:
+#     print("extracting entities")
+#     ep.select_entity_tweets(args.d,args.w,args.a)
+# if args.t:
+#     ep.select_hashtags_tweets()
+# print("ranking events")
+# ranked_events = ep.rank_events()
+# #print(ranked_events)
+# print("writing output")
+# tweetinfo = open(args.p,"w",encoding = "utf-8")
+# for tweet in ep.tweets:
+#     info = [tweet.id,tweet.user,str(tweet.date),tweet.text," ".join([str(x) for x in tweet.daterefs]),"|".join([x for x in tweet.chunks])]
+#     try:
+#         info.append(" ".join(tweet.entities))
+#         tweetinfo.write("\t".join(info) + "\n")
+#     except:
+#         tweetinfo.write("\t".join(info) + "\n")
+# tweetinfo.close()
+# eventinfo = open(args.o,"w",encoding = "utf-8")
+# for event in ranked_events:
+#     outstr = "\n" + "\t".join([str(x) for x in event[:-1]]) + "\n" + "\n".join(event[-1]) + "\n"
+#     eventinfo.write(outstr)
+# eventinfo.close()
 
 
 
