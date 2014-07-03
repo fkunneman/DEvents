2951199f380c6464509c70b2f202c6d59737010e
Florian Kunneman <f.kunneman@let.ru.nl>
2014-07-03 13:05:32 +0200

    fix

--- extract_events_daily.py
+++ extract_events_daily.py
@@ -27,6 +27,8 @@ parser.add_argument('--window', type = int, action = 'store', default = 7,
     help = "The window in days of tweets on which event extraction is based (default = 7 days)")
 args = parser.parse_args() 
 
+event_vars = [["fit",False,"events_fit.txt"],["fit",True,"events_fit_multi.txt"],["freq",False,"events_freq.txt"]]
+
 #sort input-files
 day_files = defaultdict(list)
 for infile in args.i:
@@ -48,11 +50,6 @@ for i,day in enumerate(sorted(day_files.keys())):
     ep.discard_last_day(args.window)
     if i >= 6:
         print("ranking events")
-        ranked_events_fit = ep.rank_events("fit")
-        ranked_events_freq = ep.rank_events("freq")
-        ranked_events_fit_multi = ep.rank_events("fit",clust = True)
-        #print(ranked_events)
-        print("writing output")
         basedir = args.o + day + "/"
         try:
             os.mkdir(basedir)
@@ -68,18 +65,10 @@ for i,day in enumerate(sorted(day_files.keys())):
             except:
                 tweetinfo.write("\t".join(info) + "\n")
         tweetinfo.close()
-        eventinfo_fit = open(basedir + "events_fit.txt","w",encoding = "utf-8")
-        for event in ranked_events_fit:
-            outstr = "\n" + "\t".join([str(x) for x in event[:-1]]) + "\n" + "\n".join(event[-1]) + "\n"
-            eventinfo_fit.write(outstr)
-        eventinfo_fit.close()
-        eventinfo_freq = open(basedir + "events_freq.txt","w",encoding = "utf-8")
-        for event in ranked_events_freq:
-            outstr = "\n" + "\t".join([str(x) for x in event[:-1]]) + "\n\n"
-            eventinfo_freq.write(outstr)
-        eventinfo_freq.close()
-        eventinfo_fit_multi = open(basedir + "events_fit_multi.txt","w",encoding = "utf-8")
-        for event in ranked_events_fit_multi:
-            outstr = "\n" + "\t".join([str(x) for x in event[:-1]]) + "\n" + "\n".join(event[-1]) + "\n"
-            eventinfo_fit_multi.write(outstr)
-        eventinfo_fit_multi.close()
+        for j,ev in enumerate(event_vars):
+            ranked_events = ep.rank_events(ev[0],clust=ev[1])
+            eventinfo = open(basedir + ev[2],"w",encoding = "utf-8")
+            for event in ranked_events:
+                outstr = "\n" + "\t".join([str(x) for x in event[:-1]]) + "\n" + "\n".join(event[-1]) + "\n"
+                eventinfo_fit.write(outstr)
+            eventinfo_fit.close()
