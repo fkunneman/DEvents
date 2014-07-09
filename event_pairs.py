b0bcb951bb22b3b8b9faac35917bbb14016eda87
Florian Kunneman <f.kunneman@let.ru.nl>
2014-07-09 14:09:16 +0200

    fix

--- event_pairs.py
+++ event_pairs.py
@@ -228,39 +228,41 @@ class Event_pairs:
                 pairs = itertools.combinations(indexes,2)
                 scores = [([x[0]],[x[1]],pair_sim[x[0]][x[1]]) for x in pairs]
                 print(date,events,indexes,scores)
-                scores_sorted = sorted(scores,key = lambda x : x[2],reverse = True)
-                while scores_sorted[0][2] > 0.7:
-                    highest_sim = scores_sorted[0]
-                    #merge events
-                    event1 = [x for x in events if bool(set(highest_sim[0]) & set(x.id))][0]
-                    event2 = [x for x in events if bool(set(highest_sim[1]) & set(x.id))][0]
-                    if event1.score > event2.score:
-                        event1.merge(event2)
-                        events.remove(event2)
-                        self.events.remove(event2)
-                        event = event1
-                    else:
-                        event2.merge(event1)
-                        events.remove(event1)
-                        self.events.remove(event1)
-                        event = event2
-                    #recalculate similarity graph
-                    all_s = []
-                    remove_s = []
-                    event_set = set(event.id)
-                    for score in scores:
-                        if bool(event_set & set([score[0],score[1]])):
-                            remove_s.append(score)
-                        else:
-                            all_s.extend([score[0],score[1]])
-                    for s in remove_s:
-                        scores.remove(s)
-                    for s in set(all_s):
-                        sims = [(aa, bb) for aa in event.id for bb in s]
-                        mean_sim = numpy.mean([pair_sim[x[0]][x[1]] for x in sims])
-                        scores.append((event.id,s,mean_sim))
+                if len(scores) > 0:
                     scores_sorted = sorted(scores,key = lambda x : x[2],reverse = True)
-            print([(x.id,x.entities,x.tweets) for x in events])
+                    while scores_sorted[0][2] > 0.7:
+                        highest_sim = scores_sorted[0]
+                        #merge events
+                        event1 = [x for x in events if bool(set(highest_sim[0]) & set(x.id))][0]
+                        event2 = [x for x in events if bool(set(highest_sim[1]) & set(x.id))][0]
+                        if event1.score > event2.score:
+                            event1.merge(event2)
+                            events.remove(event2)
+                            self.events.remove(event2)
+                            event = event1
+                        else:
+                            event2.merge(event1)
+                            events.remove(event1)
+                            self.events.remove(event1)
+                            event = event2
+                        #recalculate similarity graph
+                        all_s = []
+                        remove_s = []
+                        event_set = set(event.id)
+                        for score in scores:
+                            if bool(event_set & set([score[0],score[1]])):
+                                remove_s.append(score)
+                            else:
+                                all_s.extend([score[0],score[1]])
+                        for s in remove_s:
+                            scores.remove(s)
+                        for s in set(all_s):
+                            sims = [(aa, bb) for aa in event.id for bb in s]
+                            mean_sim = numpy.mean([pair_sim[x[0]][x[1]] for x in sims])
+                            scores.append((event.id,s,mean_sim))
+                        scores_sorted = sorted(scores,key = lambda x : x[2],reverse = True)
+                    print([(x.id,x.entities,x.tweets) for x in events])
+                    quit()
 
             # sorted_pairs = sorted(sim_pair.keys(),reverse = true)
             
