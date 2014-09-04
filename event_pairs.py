4faeea2eec6e7d9849f937bd8744d62bac5e38d1
Florian Kunneman <f.kunneman@let.ru.nl>
2014-09-04 11:39:17 +0200

    fix

--- event_pairs.py
+++ event_pairs.py
@@ -84,7 +84,8 @@ class Event_pairs:
                 else:
                     date = time_functions.return_datetime(tokens[2],setting="vs").date()
                 dateref_phrase = calculations.extract_date(text,date)
-                print(dateref_phrase,len(dateref_phrase))
+                if dateref_phrase:
+                    print(dateref_phrase,len(dateref_phrase))
                 if dateref_phrase:
                     if len(dateref_phrase) > 1:
                         chunks = dateref_phrase[0]
