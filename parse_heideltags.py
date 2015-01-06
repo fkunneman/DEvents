541da73cf1c8f38517309e82600c4fffcea0bc3d
Florian Kunneman <f.kunneman@let.ru.nl>
2015-01-06 14:14:49 +0100

    update

--- parse_heideltags.py
+++ parse_heideltags.py
@@ -4,7 +4,21 @@ import codecs
 import re
 
 infile = codecs.open(sys.argv[1],"r","utf-8")
-#outfile = codecs.open(sys.argv[2],"a","utf-8")
+outfile = codecs.open(sys.argv[2],"a","utf-8")
+try:
+    lax = int(sys.argv[3])
+except:
+    print "last argument should be integer"
+
+datevalue1 = re.compile(r"UNDEF-day-")
+datevalue2 = re.compile(r"UNDEF-this-week-WE")
+datevalue3 = re.compile(r"UNDEF-year-\d{1,2}-\d{1,2}")
+datevalue4 = re.compile(r"UNDEF-next-(week|month|year)")
+datevalue5 = re.compile(r"UNDEF-century\d{2}-\d{2}-\d{2}")
+datevalue6 = re.compile(r"\d{4}-\d{1,2}-\d{1,2}")
+timevalue1 = re.compile(r"UNDEF-day-")
+setvalue1 = re.compile(r"P\d(W|D|M|Y|WE)")
+setvalue2 = re.compile(r"XXXX-WXX-\dT(EV|MO|AF|NI)")
 
 lines = infile.readlines()
 infile.close()
@@ -27,12 +41,15 @@ for line in lines[start:]:
             parts = textparts[timetag].split(">")
             tex = parts[1]
             tag = parts[0]
-#            if re.search("DATE",tag):
-#                print "datevalue",tag.split("value=")[1],"tex",tex
-#            if re.search("type=\"TIME\"",tag):
-#                print "timevalue",tag.split("value=")[1],"tex",tex
-            if re.search("type=\"SET\"",tag):
-                print "timevalue",tag.split("value=")[1],"tex",tex
-#        timetags = [x.split(">")[0] for x in textparts]
- #       print textparts
+            value = tag.split("value=")[1]
+            if re.search("type=\"DATE\"",tag):
+                if datevalue1.search(value) or datevalue2.search(value) or datevalue3.search(value) or datevalue4.search(value) or datevalue5.search(value) or datevalue6.search(value):
+                    texs.append((tex,value))
+            elif re.search("type=\"TIME\"",tag):
+                if timevalue1.search(value):
+                    texs.append((tex,value))
+            elif re.search("type=\"SET\"",tag):
+                if setvalue1.search(value) or setvalue2.search(value):
+                    texs.append((tex,value))
+        print text,texs
 
