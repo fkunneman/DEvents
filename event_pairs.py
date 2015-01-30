66e77bc96ddf16bcc96f42c9ba1d56a39295bc19
Florian Kunneman <f.kunneman@gmail.com>
2015-01-30 10:24:19 +0100

    fix

--- event_pairs.py
+++ event_pairs.py
@@ -493,7 +493,7 @@ class Event_pairs:
                 rankings[x] = [i,self.entities[i]]
             print("BEFORE",[x[0] for x in self.entities])
             for i,e0 in enumerate([x[0] for x in self.entities[:-1]]):
-                scores = [[0,0]] * (len(self.entities) - (i+1))
+                scores = [[0,0] for y in itertools.repeat(None,(len(self.entities) - (i+1)))]
                 entities = [x[0] for x in self.entities[i+1:]]
             # entity_pair in itertools.combinations(self.entities,2):
             #     e0 = entity_pair[0][0]
@@ -507,13 +507,13 @@ class Event_pairs:
                         for j,e1 in enumerate(entities):
                             if re.search(re.escape(e1),text):
                                 p1 = re.search(re.escape(e1),text).span()[0]
-                                print("b",scores,p0,p1,j,scores[j])
+#                                print("b",scores,p0,p1,j,scores[j])
                                 if p0 < p1:
                                     scores[j][0] += 1
                                     #pl0 += 1
                                 else:
                                     scores[j][1] += 1
-                                print("a",scores)
+#                                print("a",scores)
                 for j,e1 in enumerate(entities):
                     score = scores[j]
                     if score[0] > score[1] and rankings[e0][0] > rankings[e1][0]:
