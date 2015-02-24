7ecc248c518f6f0e1b4749ec8f87ba7806ed79a8
Florian Kunneman <f.kunneman@let.ru.nl>
2015-02-24 15:35:04 +0100

    fda

--- calculations.py
+++ calculations.py
@@ -457,21 +457,16 @@ def merge_event_sets(set_current,set_new):
     print("new",len(set_merged))
     return set_merged
 
-def return_similarity_graph(documents,one_to_all = False):
+def return_similarity_graph(documents):
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
     cos = cosine_similarity(tfidf_matrix,tfidf_matrix)
     pair_sim = defaultdict(lambda : defaultdict(list))
     #write similarity pairs
-    if one_to_all:
-        for i,sim in enumerate(cos[0]):
-            pair_sim[]
-    else:
-        for i,document in enumerate(documents):
-            for j,sim in enumerate(cos[i]):
-                pair_sim[i][j] = cos[i][j]
-        return pair_sim
-
+    for i,document in enumerate(documents):
+        for j,sim in enumerate(cos[i]):
+            pair_sim[i][j] = cos[i][j]
+    return pair_sim
 
 def calculate_cosine_similarity(vector1,vector2):
     if len(vector1) != len(vector2):
