457d9788ac67d0c3e3a3eefe6796d3e947bd51ca
Florian Kunneman <f.kunneman@gmail.com>
2015-03-25 15:42:33 +0100

    fix

--- classify_periodicity_slwindow.py
+++ classify_periodicity_slwindow.py
@@ -25,30 +25,41 @@ infile = open(args.i,"r",encoding="utf-8")
 lines = infile.readlines()
 infile.close()
 for i,line in enumerate(lines):
+    tokens = line.strip().split("\t")
     date = time_functions.return_datetime(tokens[0],setting="vs")
     date_terms[date].extend(tokens[2].split(", "))
     for term in tokens[2].split(", "):
         term_events[term].append(line.strip())
         term_dates[term].append(date)
 
-for current_date in sorted(date_terms.keys())[:2]:
+for current_date in sorted(date_terms.keys())[:400]:
     print(current_date)
-    for term in date_term[current_date]:
-        history = [date for date in term_dates[term] if date < current_date] 
-        intervals = calculations.return_intervals(history + current_date)
+    for term in list(set(date_terms[current_date])):
+        history = [date for date in term_dates[term] if date < current_date]
+        history = history + [current_date] 
+        intervals = calculations.return_intervals(history)
+#        print("AFTER",intervals)
         if len(intervals) >= 2:
             scores = []
-            for i in range(len(intervals[:-2])):
+            for i in range(len(intervals[:-1])):
                 sequence = intervals[i:]
+                #print(sequence)
                 scores.append([i,calculations.return_relative_stdev(sequence)])
             best = sorted(scores,key = lambda x : x[0])[0]
+            if term == "appartement":
+                print(history,intervals,best,current_date)
             date_periodics[current_date].append([term,best[1],intervals,history[best[0]:]])
+            #print([term,best[1],intervals,history[best[0]:]])
 
 all_dates = set(date_terms.keys())
 all_years = set([x.year for x in all_dates])
 for year in sorted(all_years):
-    outfile = open(outdir + year + ".txt","w","utf-8")
+    outfile = open(args.o + str(year) + ".txt","w",encoding = "utf-8")
+    periodics = []
     for date in [x for x in all_dates if x.year == year]:
-        for periodic in date_periodics[date]:
-            outfile.write("\t".join(periodic[:2]) + ",".join(periodic[2]) + ",".join(periodic[3]) + "\n")
+        periodics.extend(date_periodics[date])
+    for periodic in sorted(periodics,key = lambda x : x[1]):
+        outfile.write(periodic[0] + "\t" + str(periodic[1]) + "\t" + \
+            ",".join([str(x) for x in periodic[2]]) + "\t" + \
+            ",".join([str(x) for x in periodic[3]]) + "\n")
     outfile.close()            
