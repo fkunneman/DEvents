294bfe4a5abd20008afbc862822c34503c362916
Florian Kunneman <f.kunneman@gmail.com>
2015-04-09 23:36:59 +0200

    check

--- event_classes.py
+++ event_classes.py
@@ -229,7 +229,7 @@ class Calendar:
                         #print(entity,sequence["intervals"])
                         if entity == "oudjaarsdag":
                             print(entity,"BEFORE CALK",sequence["date_info"])
-                        periodicities = calculations.return_calendar_periodicities(sequence["date_info"].copy()) 
+                        periodicities = calculations.return_calendar_periodicities(sequence["date_info"][:]) 
                         if entity == "oudjaarsdag":
                             print(entity,"AFTER CALK",sequence["date_info"])
                         if len(periodicities) > 0:
