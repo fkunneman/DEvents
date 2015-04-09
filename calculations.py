984b31321c5269d532bdbafb91e85400878988fb
Florian Kunneman <f.kunneman@let.ru.nl>
2015-04-09 18:17:37 +0200

    fix

--- calculations.py
+++ calculations.py
@@ -726,7 +726,7 @@ def score_calendar_periodicity(pattern,entries,total):
                 gap = gap_start + step
                 while gap < gap_end:
                     gap_date = dummy_date
-                    gap_date[level] = gap
+                    gap_date[sequence_level] = gap
                     gaps.append(gap_date)
                     gap += step
     return [numpy.mean([coverage,consistency]),coverage,consistency,len(sequence) + len(gaps),entries,gaps,pattern]
