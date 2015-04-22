80ba75ca715807f5a004eace9c9edc2d4a1b2fb5
Florian Kunneman <f.kunneman@let.ru.nl>
2015-04-22 17:19:35 +0200

    fix

--- calculations.py
+++ calculations.py
@@ -751,8 +751,10 @@ def apply_calendar_pattern(pattern,last_date,step):
                 while index > 0:
                     day += 7
                     index -= 1
-                print(pattern,last_date,year,month,day)
-                return_date = datetime.datetime(year,month,day)
+                try:
+                    return_date = datetime.datetime(year,month,day)
+                except:
+                    return False
 
     return return_date
 
