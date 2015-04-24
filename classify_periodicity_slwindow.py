1ecde1d24a7f11a8186a3a10e24b062a238c0baf
Florian Kunneman <f.kunneman@let.ru.nl>
2015-04-24 13:23:52 +0200

    update

--- classify_periodicity_slwindow.py
+++ classify_periodicity_slwindow.py
@@ -65,7 +65,8 @@ for i,line in enumerate(lines):
                 event_calendar.cluster_entities_periodicity(args.cluster)
                 event_calendar.predict_events(predict_to_date,0.25)
                 for entry in event_calendar.expected_events:
-                    predictfile.write("\t".join([str(entry[0]),", ".join(entry[1]),",".join([str(x) for x in [entry[2],entry[3],entry[4]]])]) + "\n")
+                    predictfile.write("\t".join([", ".join(entry[0]),entry[1],
+                        ", ".join([str(x) for x in [entry[2:]]])]) + "\n")
                 predict = False
                 predictfile.close()
     #and date <= datetime.datetime(2014,4,3):
@@ -76,153 +77,42 @@ for i,line in enumerate(lines):
 
 if args.cluster:
     #perform clustering
-    event_calendar.cluster_entities_periodicity(args.cluster)
+    event_calendar.cluster_entities_periodicity(args.cluster,args.cal)
     #write periodics to file
-    outfile = open(args.o + "calper_clusteredpattern_" + str(args.cluster)[2:] + ".txt",
-        "w",encoding="utf-8")
-    periodics = sorted(event_calendar.periodics,key = lambda x : x["score"],reverse = True)
-    for periodic in periodics:
-        outfile.write("---------------\n" + periodic["pattern"] + "\t" + 
-            ", ".join(periodic["entities"]) + "\t" + 
-            ",".join(str(x) for x in [periodic["score"],periodic["coverage"],
-            periodic["consistency"],periodic["step"],periodic["len"]]) + "\t" + 
-            " > ".join([str(x[0].date()) for x in periodic["dates"]]) + "\n" + 
-            "\n".join([e.tweets[0] for e in periodic["events"]]) + "\n")
-    outfile.close()
+    if args.cal:
+        outfile = open(args.o + "calper_clusteredpattern_" + str(args.cluster)[2:] + ".txt",
+            "w",encoding="utf-8")
+        periodics = sorted(event_calendar.periodics,key = lambda x : x["score"],reverse = True)
+        for periodic in periodics:
+            outfile.write("---------------\n" + periodic["pattern"] + "\t" + 
+                ", ".join(periodic["entities"]) + "\t" + 
+                ",".join(str(x) for x in [periodic["score"],periodic["coverage"],
+                periodic["consistency"],periodic["step"],periodic["len"]]) + "\t" + 
+                " > ".join([str(x[0].date()) for x in periodic["dates"]]) + "\n" + 
+                "\n".join([e.tweets[0] for e in periodic["events"]]) + "\n")
+        outfile.close()
+    else:
+         outfile = open(args.o + "stdev_clustered_" + str(args.cluster)[2:] + ".txt",
+            "w",encoding="utf-8")
+        periodics = sorted(event_calendar.periodics,key = lambda x : x["score"])
+        for periodic in periodics:
+            outfile.write("---------------\n" + str(periodic["score"]) + "\t" + 
+                ", ".join(periodic["entities"]) + "\t" + 
+                " > ".join([str(x) for x in periodic["dates"]]) + "\t" +
+                "-".join([str(x) for x in periodic["intervals"]]) + "\n")
+        outfile.close()       
 
-else:
-    #sort by periodicity
-    entity_periodicity = []
-    for entity in event_calendar.entity_periodicity["calendar"].keys():
-        for periodicity in event_calendar.entity_periodicity["calendar"][entity]:
-            entity_periodicity.append([entity,periodicity])
-    sorted_periodicities = sorted(entity_periodicity,key = lambda x : x[1][0],reverse = True)
+# else:
+#     #sort by periodicity
+#     entity_periodicity = []
+#     for entity in event_calendar.entity_periodicity["calendar"].keys():
+#         for periodicity in event_calendar.entity_periodicity["calendar"][entity]:
+#             entity_periodicity.append([entity,periodicity])
+#     sorted_periodicities = sorted(entity_periodicity,key = lambda x : x[1][0],reverse = True)
 
-    outfile = open(args.o + "calper_2014_cl.txt","w",encoding = "utf-8")
-    for p in sorted_periodicities:
-        outfile.write("---------------\n" + p[0] + "\t" + p[1][-1] + "\t" + \
-            ", ".join([str(x) for x in p[1][1:5]]) + "\t" + " > ".join([str(x[0]) for x in p[1][5]]) + \
-            "\t" + ", ".join([str(x[0]) for x in p[1][6]]) + "\n")
-    outfile.close()
-
-# tps = [[k,term_periodicity[k]] for k in term_periodicity.keys()]
-# sorted_term_periodicity = sorted(tps,key = lambda x : x[1][0])
-# sorted_term_periodicity_cutoff = []
-# for tp in sorted_term_periodicity:
-#     if tp[1][0] < 25:
-#         sorted_term_periodicity_cutoff.append(tp)
-#     else:
-#         break
-
-# outfile = open(args.o + "baseline_2014_firsthalf.txt","w",encoding = "utf-8")
-# for tp in sorted_term_periodicity:
-#     outfile.write(tp[0] + "\t" + str(tp[1][0]) + "\t" + ",".join([str(x) for x in tp[1][1]]) + "\t" + ",".join([str(x) for x in tp[1][2]]) + "\n")
-# outfile.close()
-
-#outfile = open(args.o + "baseline_2014_jaccard_k10.txt","w",encoding = "utf-8")
-#terms = [x[0] for x in sorted_term_periodicity_cutoff]
-#term_candidates = {}
-#for term in terms:
-#    term_candidates[term] = term_periodicity[term][3]
-#    #print(term,term_candidates[term])
-
-# clusters = calculations.cluster_jp(term_candidates,10)
-# infoclusters = []
-# for cluster in clusters.keys():
-#     terms = clusters[cluster]
-#     info = [[term,term_periodicity[term][0],term_periodicity[term][1]] for term in terms]
-#     best_stdev = min([x[1] for x in info])
-#     infoclusters.append([best_stdev,info])
-# sorted_infoclusters = sorted(infoclusters,key = lambda x : x[0])
-# for clust in sorted_infoclusters:
-#     outfile.write("-------" + str(clust[0]) + "-------\n")
-#     for term in clust[1]:
-#         outfile.write(term[0] + "\t" + str(term[1]) + "\t" + ",".join([str(x) for x in term[2]]) + "\n")
-# outfile.close()
-
-
-# entity_candidates = [[x for x in event_calendar.entity_sequences[term]["entities"] if x in terms] for term in terms]
-# dateseqs = [x[1][1] for x in sorted_term_periodicity_cutoff]
-# clusters = calculations.cluster_time_vectors(terms,dateseqs,entity_candidates,datetime.datetime(2010,12,1),datetime.datetime(2014,7,1),3)
-# infoclusters = []
-# for cluster in clusters.keys():
-#     vecs = clusters[cluster]
-#     info = [sorted_term_periodicity_cutoff[x] for x in vecs]
-#     terms = [x[0] for x in info]
-#     stdevs = sorted([x[1][0] for x in info])
-#     dateseqs = [",".join([str(d) for d in x[1][1]]) for x in info]
-#     intervals = [",".join([str(i) for i in x[1][2]]) for x in info]
-#     infoclusters.append([",".join(terms),stdevs,"---".join(dateseqs),"---".join(intervals)])
-# sorted_info_clusters = sorted(infoclusters,key = lambda x : x[1][0])
-# for clust in sorted_info_clusters:
-#     outfile.write(clust[0] + "\t" + ",".join([str(x) for x in clust[1]]) + "\t" + clust[2] + "\t" + clust[3] + "\n")
-# outfile.close()
-
-#clusterterms
-# outfile = open(args.o + "baseline_2014_firsthalf_clustered.txt","w",encoding = "utf-8")
-# terms = [x[0] for x in sorted_term_periodicity_cutoff]
-# entity_candidates = [[x for x in event_calendar.entity_sequences[term]["entities"] if x in terms] for term in terms]
-# dateseqs = [x[1][1] for x in sorted_term_periodicity_cutoff]
-# clusters = calculations.cluster_time_vectors(terms,dateseqs,entity_candidates,datetime.datetime(2010,12,1),datetime.datetime(2014,7,1),3)
-# infoclusters = []
-# for cluster in clusters.keys():
-#     vecs = clusters[cluster]
-#     info = [sorted_term_periodicity_cutoff[x] for x in vecs]
-#     terms = [x[0] for x in info]
-#     stdevs = sorted([x[1][0] for x in info])
-#     dateseqs = [",".join([str(d) for d in x[1][1]]) for x in info]
-#     intervals = [",".join([str(i) for i in x[1][2]]) for x in info]
-#     infoclusters.append([",".join(terms),stdevs,"---".join(dateseqs),"---".join(intervals)])
-# sorted_info_clusters = sorted(infoclusters,key = lambda x : x[1][0])
-# for clust in sorted_info_clusters:
-#     outfile.write(clust[0] + "\t" + ",".join([str(x) for x in clust[1]]) + "\t" + clust[2] + "\t" + clust[3] + "\n")
-# outfile.close()
-
-
-#for es in event_calendar.entity_sequences.keys():
-#    print(es,"\t",event_calendar.entity_sequences[es])
-
-# outfile = open(args.o + "test.txt","w",encoding = "utf-8")
-# periodicities = []
-# for term in event_calendar.term_stdev.keys():
-#     for stdev in event_calendar.term_stdev[term]:
-#         periodicities.append([stdev[0]] + [term] + [",".join([str(x) for x in stdev[1]])] + [",".join([str(x) for x in stdev[2]])] + [",".join([str(x) for x in event_calendar.term_sequences[term]["intervals"]])])
-# sorted_periodicities = sorted(periodicities,key = lambda x : x[0])
-# for per in sorted_periodicities:
-#     outfile.write(str(per[0]) + "\t" + "\t".join(per[1:]) + "\n")
-
-
-#for string in event_calendar.string_events.keys():
-#    print([x.entities for x in event_calendar.string_events[string]])
-
-
-#     date_terms[date].extend()
-#     for term in tokens[2].split(", "):
-#         term_date_events[term][date].append(line.strip())
-#         term_dates[term].append(date)
-
-# for current_date in sorted(date_terms.keys())[:400]:
-#     print(current_date)
-#     for term in list(set(date_terms[current_date])):
-#         history = [date for date in term_dates[term] if date < current_date] + [current_date]
-#         intervals = calculations.return_intervals(history)
-#         if len(intervals) >= 2:
-#             scores = []
-#             for i in range(len(intervals[:-1])):
-#                 sequence = intervals[i:]
-#                 scores.append([i,calculations.return_relative_stdev(sequence)])
-#             best = sorted(scores,key = lambda x : x[0])[0]
-#             date_periodics[current_date].append([term,best[1],intervals,history[best[0]:]])
-
-# all_dates = set(date_terms.keys())
-# all_years = set([x.year for x in all_dates])
-# for year in sorted(all_years):
-#     outfile = open(args.o + str(year) + ".txt","w",encoding = "utf-8")
-#     periodics = []
-#     for date in [x for x in all_dates if x.year == year]:
-#         periodics.extend(date_periodics[date])
-#     for periodic in sorted(periodics,key = lambda x : x[1]):
-#         outfile.write(periodic[0] + "\t" + str(periodic[1]) + "\t" + \
-#             ",".join([str(x) for x in periodic[2]]) + "\t" + \
-#             ",".join([str(x) for x in periodic[3]]) + "\n")
-#     outfile.close()            
+#     outfile = open(args.o + "calper_2014_cl.txt","w",encoding = "utf-8")
+#     for p in sorted_periodicities:
+#         outfile.write("---------------\n" + p[0] + "\t" + p[1][-1] + "\t" + \
+#             ", ".join([str(x) for x in p[1][1:5]]) + "\t" + " > ".join([str(x[0]) for x in p[1][5]]) + \
+#             "\t" + ", ".join([str(x[0]) for x in p[1][6]]) + "\n")
+#     outfile.close()
--- event_classes.py
+++ event_classes.py
@@ -207,7 +207,7 @@ class Calendar:
                             len(sequence["intervals"]):
                             stdev = calculations.return_relative_stdev(sequence["intervals"])
                             self.entity_periodicity["stdev"][entity] = [stdev,
-                            list(set([x[0] for x in sequence["dates_events"]])),sequence["intervals"]]
+                                list(set([x[0] for x in sequence["dates_events"]])),sequence["intervals"]]
                     if calc:
                         if not (len(sequence["intervals"]) > 15 and \
                             (sequence["intervals"].count(1) / len(sequence["intervals"])) > 0.3):
@@ -219,104 +219,144 @@ class Calendar:
                                 self.entity_periodicity["calendar"][entity] = periodicities
             sequence["dates_events"].append([event.date,event])
 
-    def cluster_entities_periodicity(self,cluster_threshold):
+    def cluster_entities_periodicity(self,cluster_threshold,calper = True):
         print("generating bigdocs")
         #generate bigdocs per entity
         all_entities = self.entity_sequences.keys()
-        entities = self.entity_periodicity["calendar"].keys()
-        entities_nopattern = list(set(all_entities)-set(entities))
+        if calper:
+            entities = self.entity_periodicity["calendar"].keys()
+            all_entities = list(set(all_entities)-set(entities))
         entity_index = defaultdict(lambda : {})
         index_entity = {}
         documents = []
         i = 0
         #entities without periodicity pattern
-        for entity in entities_nopattern:
+        for entity in all_entities:
             #assign index
             entity_index[entity]["all"] = i
             index_entity[i] = [entity,"all"]
             i += 1
             #make document
             documents.append(" ".join([" ".join(x.tweets) for \
-                x in self.entity_sequences[entity]["events"]]))            
-        #entities with periodicity pattern
-        for entity in entities:
-            periodicities = self.entity_periodicity["calendar"][entity]
-            for per in periodicities:
-                #assign index
-                pattern = per[-1]
-                entity_index[entity][pattern] = i
-                index_entity[i] = [entity,pattern]
-                i += 1
-                #make document
-                dates = [x[0] for x in per[5]]
-                documents.append(" ".join([" ".join(x.tweets) for \
-                    x in self.entity_sequences[entity]["events"] if x.date in dates]))                  
+                x in self.entity_sequences[entity]["events"]]))      
+        if calper:      
+            #entities with periodicity pattern
+            for entity in entities:
+                periodicities = self.entity_periodicity["calendar"][entity]
+                for per in periodicities:
+                    #assign index
+                    pattern = per[-1]
+                    entity_index[entity][pattern] = i
+                    index_entity[i] = [entity,pattern]
+                    i += 1
+                    #make document
+                    dates = [x[0] for x in per[5]]
+                    documents.append(" ".join([" ".join(x.tweets) for \
+                        x in self.entity_sequences[entity]["events"] if x.date in dates]))                  
         vectors = calculations.tfidf_docs(documents)
         print("calculating similarities")
         pairsims = calculations.return_similarities(vectors,vectors)
         print("grouping entities")
-        #group entities
-        pattern_entities = defaultdict(list)
-        patterns = []
-        for entity in entities:
-            entity_patterns = [x[-1] for x in self.entity_periodicity["calendar"][entity]]
-            patterns.extend(entity_patterns)
-            for pattern in entity_patterns:
-                pattern_entities[pattern].append(entity)
-        patterns = list(set(patterns))
-        for pattern in patterns:
-            ents = list(set(pattern_entities[pattern]))
-            if len(ents) > 1:
-                indices = [entity_index[x][pattern] for x in ents]
-                clusters = calculations.cluster_documents(pairsims,indices,cluster_threshold)
-                groups = []
-                for cluster in clusters:
-                    groups.append([index_entity[x][0] for x in cluster])
-            else:
-                groups = [ents]
-            #print(pattern,[g for g in groups if len(g) > 1])
-            #print(groups)
-            for group in groups:
-                if len(group) > 1: #rescore periodicity
-                    all_dates = []
-                    periodic_dates = []
-                    events = []
-                    event_ids = []
-                    for entity in group:
-                        dates_events = self.entity_sequences[entity]["dates_events"]
-                        all_dates.extend([x[0] for x in dates_events])
-                        for e in [x[1] for x in dates_events]:
-                            if not e.ids[0] in event_ids:
-                                event_ids.append(e.ids[0])
-                                events.append(e)
-                        periodic = [x for x in self.entity_periodicity["calendar"][entity] if \
-                            x[7] == pattern][0]
-                        periodic_dates.extend(periodic[5])
-                    listpattern = pattern[1:-1].split(",")
-                    for i,lp in enumerate(listpattern):
-                        if re.search(r"\d",lp):
-                            listpattern[i] = int(lp)
-                    unique_dates = []
-                    unique_periodic_dates = []
-                    for pd in periodic_dates:
-                        if not pd[0] in unique_dates:
-                            unique_periodic_dates.append(pd)
-                            unique_dates.append(pd[0])
-                    events = [x for x in events if x.date in unique_dates]
-                    #print(group)                     
-                    new_periodic = calculations.score_calendar_periodicity(listpattern,
-                        copy.deepcopy(unique_periodic_dates),len(list(set(all_dates))))
+        if calper:
+            #group entities
+            pattern_entities = defaultdict(list)
+            patterns = []
+            for entity in entities:
+                entity_patterns = [x[-1] for x in self.entity_periodicity["calendar"][entity]]
+                patterns.extend(entity_patterns)
+                for pattern in entity_patterns:
+                    pattern_entities[pattern].append(entity)
+            patterns = list(set(patterns))
+            for pattern in patterns:
+                ents = list(set(pattern_entities[pattern]))
+                if len(ents) > 1:
+                    indices = [entity_index[x][pattern] for x in ents]
+                    clusters = calculations.cluster_documents(pairsims,indices,cluster_threshold)
+                    groups = []
+                    for cluster in clusters:
+                        groups.append([index_entity[x][0] for x in cluster])
                 else:
-                    #print(group)
-                    new_periodic = [x for x in self.entity_periodicity["calendar"][group[0]] if \
+                    groups = [ents]
+                for group in groups:
+                    if len(group) > 1: #rescore periodicity
+                        all_dates = []
+                        periodic_dates = []
+                        events = []
+                        event_ids = []
+                        for entity in group:
+                            dates_events = self.entity_sequences[entity]["dates_events"]
+                            all_dates.extend([x[0] for x in dates_events])
+                            for e in [x[1] for x in dates_events]:
+                                if not e.ids[0] in event_ids:
+                                    event_ids.append(e.ids[0])
+                                    events.append(e)
+                            periodic = [x for x in self.entity_periodicity["calendar"][entity] if \
                                 x[7] == pattern][0]
-                    unique_dates = list(set([x[0] for x in new_periodic[5]]))
-                    events = [x[1] for x in self.entity_sequences[group[0]]["dates_events"] if \
-                        x[1].date in unique_dates] 
-                self.periodics.append({"score":new_periodic[0],"coverage":new_periodic[1],
-                    "consistency":new_periodic[2],"step":new_periodic[3],"len":new_periodic[4],
-                    "dates":new_periodic[5],"gaps":new_periodic[6],"pattern":pattern,
-                    "events":events,"entities":group})
+                            periodic_dates.extend(periodic[5])
+                        listpattern = pattern[1:-1].split(",")
+                        for i,lp in enumerate(listpattern):
+                            if re.search(r"\d",lp):
+                                listpattern[i] = int(lp)
+                        unique_dates = []
+                        unique_periodic_dates = []
+                        for pd in periodic_dates:
+                            if not pd[0] in unique_dates:
+                                unique_periodic_dates.append(pd)
+                                unique_dates.append(pd[0])
+                        events = [x for x in events if x.date in unique_dates]
+                        #print(group)                     
+                        new_periodic = calculations.score_calendar_periodicity(listpattern,
+                            copy.deepcopy(unique_periodic_dates),len(list(set(all_dates))))
+                    else:
+                        #print(group)
+                        new_periodic = [x for x in self.entity_periodicity["calendar"][group[0]] if \
+                                    x[7] == pattern][0]
+                        unique_dates = list(set([x[0] for x in new_periodic[5]]))
+                        events = [x[1] for x in self.entity_sequences[group[0]]["dates_events"] if \
+                            x[1].date in unique_dates] 
+                    self.periodics.append({"score":new_periodic[0],"coverage":new_periodic[1],
+                        "consistency":new_periodic[2],"step":new_periodic[3],"len":new_periodic[4],
+                        "dates":new_periodic[5],"gaps":new_periodic[6],"pattern":pattern,
+                        "events":events,"entities":group})
+        #stdev
+        else:
+            candidates = self.entity_periodicity["stdev"].keys()
+            while len(candidates) > 0:
+                candidate = candidates[0]
+                cdates = set([x[0] for x in self.entity_periodicity["stdev"][candidate][1]])
+                mean_intervals = numpy.mean(self.entity_periodicity["stdev"][candidate][-1])
+                group_candidates = [x for x in candidates if len(list(cdates & \
+                    set([x[0] for x in self.entity_periodicity["stdev"][x][1]]))) > 2 and (len(list(cdates & \
+                    set([x[0] for x in self.entity_periodicity["stdev"][x][1]]))) / len(cdates)) > 0.5 and \
+                    (len(list(cdates & set([x[0] for x in self.entity_periodicity["stdev"][x][1]]))) / \
+                        len(self.entity_periodicity["stdev"][x][1])) > 0.5 and \
+                    abs(numpy.mean(self.entity_periodicity["stdev"][x][-1]) - mean_intervals) < 10] 
+                #make group
+                ents = [candidate] + group_candidates
+                if len(ents) > 1:
+                    groups = []
+                    indices = [entity_index[x]["all"] for x in ents]
+                    clusters = calculations.cluster_documents(pairsims,indices,cluster_threshold)
+                    for cluster in clusters:
+                        groups.append([index_entity[x][0] for x in cluster])
+                else:
+                    groups = [ents]
+                for group in groups:
+                    if len(group) > 1: #rescore periodicity
+                        entities_stdev = [[x,self.entity_periodicity["stdev"][x]] for x in group]
+                        score = min([x[1][0] for x in entities_stdev])
+                        dates = []
+                        for e in entities_stdev:
+                            dates.extend(e[1][0])
+                        dates = list(set(dates))
+                        intervals = calculations.return_intervals(dates)
+                        self.periodics.append({"score":score,"len":len(dates),"dates":dates,
+                            "intervals":intervals,"entities":group})
+                    else:
+                        e = self.entity_periodicty["stdev"][group[0]]
+                        self.periodics.append({"score":e[0],"len":len(e[1]),
+                            "dates":[x[0] for x in e[1]],
+                            "intervals":e[2],"entities":group})
 
     def predict_events(self,until_date,threshold):
         #select above threshold patterns
@@ -347,4 +387,5 @@ class Calendar:
                     for date in extentions:
                         event = Event("x",[date,periodic["entities"],"-",[]])
                         event.set_periodics(periodic["events"])
-                        self.expected_events.append([date,periodic["entities"],periodic["score"],periodic["coverage"],periodic["consistency"]])
+                        self.expected_events.append([periodic["entities"],periodic["pattern"],last_date,date,
+                            periodic["step"],periodic["score"],periodic["coverage"],periodic["consistency"]])
