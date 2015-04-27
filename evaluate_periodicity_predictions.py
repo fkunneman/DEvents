39afecfc1a842a9ea788ebdadc53fb815f532efc
Florian Kunneman <f.kunneman@gmail.com>
2015-04-27 12:31:13 +0200

    fix

--- evaluate_periodicity_predictions.py
+++ evaluate_periodicity_predictions.py
@@ -20,10 +20,14 @@ predictfile = open(args.p,"r",encoding="utf-8")
 
 #generate term_predictions dict from file
 terms_predictions = defaultdict(list)
-for line in predictfile.readline():
+for line in predictfile.readlines():
     tokens = line.strip().split("\t")
-    terms = sorted(tokens[0].split(", "))
-    fields = tokens[2].split(", ")
-    print(fields)
-    quit()
+    terms = "_".join(sorted(tokens[0].split(", ")))
+    fields = tokens[2][1:-1].split(", ")
+    predict_date = datetime.datetime(int(fields[5][-4:]),int(fields[6]),int(fields[7]))
+    score = float(fields[11])
+    coverage = float(fields[12])
+    consistency = float(fields[13])
+    terms_predictions[terms].append((predict_date,score,coverage,consistency))
 
+print(terms_predictions)
